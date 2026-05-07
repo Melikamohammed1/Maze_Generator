88f49262510d734e5835febc2f5c84193a08d4cd
Melika <melikamoh@gmail.com>
2026-05-07 23:36:23 +0300

    Added maze wall data structure

--- maze.py
+++ maze.py
@@ -17,3 +17,12 @@ wall_t.color("white")
 wall_t.penup()
 
 turtle.done()
+# ---------------- CONFIG ---------------- #
+ROWS = 20
+COLS = 25
+CELL_SIZE = 24
+
+# ---------------- DATA ---------------- #
+northWall = [[1] * COLS for _ in range(ROWS)]
+eastWall = [[1] * COLS for _ in range(ROWS)]
+visited = [[False] * COLS for _ in range(ROWS)]
